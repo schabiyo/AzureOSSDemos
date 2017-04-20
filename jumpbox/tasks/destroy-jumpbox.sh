95d69dca1708e4ec788d9e09e37dd2048875c7e9
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-20 13:22:30 -0400

    updated jumpbox destruction task

---
+++ jumpbox/tasks/destroy-jumpbox.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e -x
+
+# 1-Login to Azure using the az command line
+echo "Logging in to Azure"
+
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
+
+echo "Validating the account name: ${storage_account_prefix}storage"
+
+# 2. switchinh to the default subscription
+
+az account set --subscription "$subscription_id"
+
+echo "Validation successfully completed"
+
+az group delete -n "$utility_rg" --yes
+
+
+ls
+
+
