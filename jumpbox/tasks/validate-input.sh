6b07411941b9c35bfec177839fde37816d9e1187
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-20 11:31:45 -0400

    update

--- jumpbox/tasks/validate-input.sh
+++ jumpbox/tasks/validate-input.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 set -e -x
 
-echo "Validating the account name..."
-
-
-echo "Validating the account name: $storage_account_prefix-storage"
-
-az storage account check-name --name "$storage_account_prefix-storage"
-
 # 1-Login to Azure using the az command line
 echo "Logging in to Azure"
 
 az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
+
+echo "Validating the account name: $storage_account_prefix-storage"
+
+az storage account check-name --name "$storage_account_prefix-storage"
 
 # 2. switchinh to the default subscription
 
