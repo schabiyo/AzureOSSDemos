00d09f85bd5e7770d6a663efdc6c0d8caf399245
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-20 01:35:45 -0400

    parameterizing the validation task

--- jumpbox/tasks/validate-input.sh
+++ jumpbox/tasks/validate-input.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e -x
 
-SERVICEPRINCIPAL= SERVICEPRINCIPAL_PWD= TENANT_ID= SUBSCRIPTION_ID=
-
-
-
 
 echo "Validating the supplied parameters"
 
@@ -13,11 +9,11 @@ echo "$subscription_id"
 # 1-Login to Azure using the az command line
 echo "Logging in to Azure"
 
-az login --service-principal -u http://azure-cli-2016-08-05-14-31-15 -p VerySecret --tenant contoso.onmicrosoft.com
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
 
 # 2. switchinh to the default subscription
 
-az account set --subscription "$newsubscription"
+az account set --subscription "$subscription_id"
 
 # 3. Check the validity of the name (no dashes, spaces, less than 8 char, no special chars etc..)
 ls
