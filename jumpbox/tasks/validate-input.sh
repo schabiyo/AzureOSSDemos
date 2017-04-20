938c87deee7a72023a71b3f10de27d525ed1298a
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-20 00:59:34 -0400

    updated validation task script

--- jumpbox/tasks/validate-input.sh
+++ jumpbox/tasks/validate-input.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 set -e -x
 
+
 echo "Validating the supplied parameters"
 
+# 1-Login to Azure using the az command line
+echo "Logging in to Azure"
+
+az login --service-principal -u http://azure-cli-2016-08-05-14-31-15 -p VerySecret --tenant contoso.onmicrosoft.com
+
+# 2. switchinh to the default subscription
+
+az account set --subscription "$newsubscription"
+
+# 3. Check the validity of the name (no dashes, spaces, less than 8 char, no special chars etc..)
 ls
 
 
