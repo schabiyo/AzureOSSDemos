c79daf88639e49af00adfd6bda5dae424469b354
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-20 14:15:02 -0400

    updated jumpbox creation task

--- jumpbox/tasks/create-jumpbox.sh
+++ jumpbox/tasks/create-jumpbox.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e -x
 
+GREEN='\033[0;32m'
+RESET="\e[0m"
 # 1-Login to Azure using the az command line
 echo "Logging in to Azure"
 
@@ -16,7 +18,7 @@ echo "Creating the resource group: $utility_rg"
 #Make a copy of the template file
 cp azure-oss-demos/environment/ossdemo-utility-template.json azure-oss-demos/environment/ossdemo-utility.json -f
 #MODIFY line in JSON TEMPLATES
-sed -i -e "s@VALUEOF-UNIQUE-SERVER-PREFIX@${server_prefix}@g" azure-oss-demos/environment/ossdemo-utility.json
+sed -i -e "s@VALUEOF-UNIQUE-SERVER-PREFIX@${jumpbox_prefix}@g" azure-oss-demos/environment/ossdemo-utility.json
 sed -i -e "s@VALUEOF-UNIQUE-STORAGE-PREFIX@${storage_account_prefix}@g" azure-oss-demos/environment/ossdemo-utility.json
 
 #BUILD RESOURCE GROUPS
@@ -30,6 +32,37 @@ echo ".APPLY JSON Template"
 echo "..Starting:"$(date)
 echo '..Applying Network Security Group for utility Resource Group'
 az group deployment create --resource-group "$utility_rg" --name InitialDeployment --template-file azure-oss-demos/environment/ossdemo-utility.json
+
+echo "Creating the Jumpbox VM"
+#Get the SSH key from the configs adn add it to the ssh folder
+echo $jumpbox_ssh_private_key >> ~/.ssh/jumpbox_${server_prefix}_id_rsa
+echo $jumpbox_ssh_public_key >> ~/.ssh/jumpbox_${server_prefix}_id_rsa.pub
+# Add this to the config file
+echo -e "Host=jumpbox-${jumpbox_prefix}.${location}.cloudapp.azure.com\nIdentityFile=~/.ssh/jumpbox_${jumpbox_prefix}_id_rsa\nUser=${jumpbox_admin}" >> ~/.ssh/config
+chmod 600 ~/.ssh/config
+chmod 600 ~/.ssh/jumpbox*
+#Delete the host name in case it already exists
+ssh-keygen -R "jumpbox-${jumpbox_prefix}.$location.cloudapp.azure.com"
+sshpubkey=$(< ~/.ssh/jumpbox_${jumpbox_prefix}_id_rsa.pub)
+
+ #CREATE UTILITY JUMPBOX SERVER
+ echo ""
+ echo "Creating CENTOS JUMPBOX utility machine for RDP and ssh"
+ echo ".Starting:"$(date)
+ echo ".Reading ssh key information from local jumpbox_${jumpbox_prefix}_id_rsa file"
+ echo ".--------------------------------------------"
+ azcreatecommand="-g ossdemo-utility -n jumpbox-${jumpbox_prefix} --public-ip-address-dns-name jumpbox-${jumpbox_prefix} \
+    --os-disk-name jumpbox-${jumpbox_prefix}-disk --image OpenLogic:CentOS:7.2:latest \
+    --nsg NSG-ossdemo-utility  \
+    --storage-sku Premium_LRS --size Standard_DS2_v2 \
+    --vnet-name ossdemos-vnet --subnet ossdemo-utility-subnet \
+    --admin-username ${jumpbox_admin} \
+    --ssh-key-value ~/.ssh/jumpbox_${jumpbox_prefix}_id_rsa.pub "
+
+echo "..Calling creation command: az vm create ${azcreatecommand}"
+echo -e "${BOLD}...Creating Jumpbox server...${RESET}"
+
+az vm create ${azcreatecommand}
 
 
 
