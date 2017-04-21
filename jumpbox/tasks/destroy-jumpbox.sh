4803fff2cf4259c55f17703f0a683868d797ff62
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-21 17:09:31 -0400

    adding output folder to pass the key along between containers

--- jumpbox/tasks/destroy-jumpbox.sh
+++ jumpbox/tasks/destroy-jumpbox.sh
@@ -1,23 +1,10 @@
 #!/bin/sh
 set -e -x
 
-GREEN='\033[0;32m'
-
-# 1-Login to Azure using the az command line
-echo "Logging in to Azure"
-
 az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
-
-echo "Validating the account name: ${storage_account_prefix}storage"
-
-# 2. switchinh to the default subscription
 
 az account set --subscription "$subscription_id"
 
-echo "Validation successfully completed"
-
 az group delete -n "$utility_rg" --yes
-
-echo -e " ${GREEN} Jumpbox successfully deleted!!!!"
 
 
