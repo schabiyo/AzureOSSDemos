7d17d0d03ff155292e561eaf43d68e73e510523b
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-23 12:48:18 -0400

    adding function to retrieve an access token by SPwq

--- jumpbox/tasks/create-oms-workspace.sh
+++ jumpbox/tasks/create-oms-workspace.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e -x
 
 source azure-oss-demos-ci/utils/pretty-echo.sh
@@ -8,9 +8,9 @@ MESSAGE="Getting an access token from AAD" ; simple_blue_echo
 
 # call function
 echo "Calling getToken()"
-getToken "72f988bf-86f1-41af-91ab-2d7cd011db47" "01277144-4fa7-48d6-ba56-450eb59cdbc5" "eoGF7TRTAWhYK" response
+getToken "72f988bf-86f1-41af-91ab-2d7cd011db47" "01277144-4fa7-48d6-ba56-450eb59cdbc5" "eoGF7TRTAWhYK" token
 
-echo "response from function:" $response
+echo "response from function:" $token
 
 
 MESSAGE="TODO: Create a Workspace " ; simple_blue_echo
