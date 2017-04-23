c3e04ed85f3efe1f3a9e5fe212873072a130aec2
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-23 15:04:55 -0400

    workspace creation task

--- jumpbox/tasks/create-oms-workspace.sh
+++ jumpbox/tasks/create-oms-workspace.sh
@@ -15,12 +15,12 @@ MESSAGE="Creating hte worksapce Workspace " ; simple_blue_echo
  CURL_COMMAND=" -H 'Host: management.azure.com' -H 'Content-Type: application/json' -H 'Authorization: Bearer OAUTH-TOKEN' -X PUT -d '{\"properties\": {\"source\": \"Azure\",\"customerId\": \"\",\"portalUrl\": \"\",\"provisioningState\": \"\",\"sku\": {\"name\": \"OMS-WORKSPACE-SKU\"},\"features\": {\"legacy\": 0,\"searchVersion\": 0}},\"id\": \"\",\"name\": \"OMS-WORKSPACE-NAME\",\"type\": \"Microsoft.OperationalInsights/workspaces\",\"location\": \"RESOURCE-LOCATION\"}' https://management.azure.com/subscriptions/SUBSCRIPTION-ID/resourcegroups/RESOURCE-GROUP-NAME/providers/Microsoft.OperationalInsights/workspaces/OMS-WORKSPACE-NAME?api-version=2015-11-01-preview"
 
 NEW_CURL_COMMAND=$(sed  "s@OAUTH-TOKEN@${token}@g" <<< $CURL_COMMAND)
-NEW_CURL_COMMAND=$(sed  "s@TENANT-ID@${tenant_id}@g" <<< $CURL_COMMAND)
+NEW_CURL_COMMAND=$(sed  "s@TENANT-ID@${tenant_id}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@CLIENT-ID@${client_id}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@CLIENT-SECRET@${client_secret}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@OMS-WORKSPACE-SKU@${oms_workspace_sku}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@OMS-WORKSPACE-NAME@${oms_workspace_sku}@g" <<< $NEW_CURL_COMMAND)
-NEW_CURL_COMMAND=$(sed  "s@RESOURCE-GROUP-NAME@${utility_rg}@g" <<< $CURL_COMMAND)
+NEW_CURL_COMMAND=$(sed  "s@RESOURCE-GROUP-NAME@${utility_rg}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@RESOURCE-LOCATION@${client_id}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@SUBSCRIPTION-ID@${subscription_id}@g" <<< $NEW_CURL_COMMAND)
 
