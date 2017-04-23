8fab51f838b8db22a589887939c449eede71f49d
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-22 23:29:56 -0400

    added oms workspace creation task

---
+++ jumpbox/tasks/create-acr.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e -x
+
+source azure-oss-demos-ci/utils/pretty-echo.sh
+
+MESSAGE="TODO InstallPowershell for Linux" ; simple_red_echo
+
+
+
+MESSAGE="TODO: Create ACR " ; simple_blue_echo
