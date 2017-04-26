aabde59288d0b488ad3a6a77f9be838df2db1da0
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-26 10:19:34 -0400

    added tassk to install rdp tools on the jumpbox

---
+++ jumpbox/tasks/configure-rdp-tools.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e -x
+
+#Script Formatting
+RESET="\e[0m"
+INPUT="\e[7m"
+BOLD="\e[4m"
+
+source azure-oss-demos-ci/utils/pretty-echo.sh
+
+
+MESSAGE="Installing RDP tools on the Jumpbox " ; simple_blue_echo
+
+mkdir  ~/.ssh/
+
+cp keys-folder/* ~/.ssh/
+cp -f ansible-configs/hosts azure-oss-demos-ci/ansible/hosts
+
+ansiblecommand=" -i hosts ../../ansible-configs/playbook-configure-rdp-tools.yml --private-key ~/.ssh/jumpbox_${jumpbox_prefix}_id_rsa"
+#we need to run ansible-playbook in the same directory as the CFG file.  Go to that directory then back out...
+cd azure-oss-demos-ci/ansible
+    ansible-playbook ${ansiblecommand}
+cd ..
+
+MESSAGE="Ansible commande successfully completed" ; simple_green_echo
+
