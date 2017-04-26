b5052545d279a3048226da36f67833adf20ec08a
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-26 18:26:40 -0400

    acr creation task

--- iaas/tasks/create-iaas-vms.sh
+++ iaas/tasks/create-iaas-vms.sh
@@ -163,10 +163,10 @@ MESSAGE=" Installing Docker on the VMs using ansible" ; simple_blue_echo
 # Updatethe Host file with the 2 server host
 # we need to make sure we run the ansible playbook from this directory to pick up the cfg file
 #May be just create the hosts file on the fly
-printf "%s\n" "[dockerhosts]" >> azure-ossdemo-git/ansible/docker-hosts
+printf "%s\n" "'[dockerhosts']" >> azure-ossdemo-git/ansible/docker-hosts
 printf "%s\n" "web1-$server_prefix" >> azure-ossdemo-git/ansible/docker-hosts
 printf "%s\n" "web2-$server_prefix" >> azure-ossdemo-git/ansible/docker-hosts
-printf "%s\n" "[buildbox]" >> azure-ossdemo-git/ansible/docker-hosts
+printf "%s\n" "'[buildbox]'" >> azure-ossdemo-git/ansible/docker-hosts
 printf "%s\n" "localhost" >> azure-ossdemo-git/ansible/docker-hosts
 
 cd azure-ossdemo-git/ansible/ 
