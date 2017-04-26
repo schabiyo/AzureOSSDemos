d3317f71a17da65e94851b174d6469b90ac5d0c7
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-26 10:19:07 -0400

    added tassk to destroy docker vms

---
+++ iaas/tasks/destroy-docker-vms.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e -x
+
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
+
+az account set --subscription "$subscription_id"
+
+az group delete -n "$iaas_rg" --yes
+
+
