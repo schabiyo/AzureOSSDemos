7b4f37e1766c2d47200701828a40beba6bc9cea3
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-26 02:07:53 -0400

    cleanup

--- utils/getWorkspaceItem.sh
+++ utils/getWorkspaceItem.sh
@@ -11,8 +11,6 @@ getWorkspaceItemStatus() {
   local subscription_id=$4
   local responsevar=$5
 
-  MESSAGE="Creating the Workspace " ; simple_blue_echo
-
   CURL_COMMAND=" -H 'Host: management.azure.com' -H 'Content-Type: application/json' -H 'Authorization: Bearer OAUTH-TOKEN' -X GET  https://management.azure.com/subscriptions/SUBSCRIPTION-ID/resourcegroups/RESOURCE-GROUP-NAME/providers/Microsoft.OperationalInsights/workspaces/OMS-WORKSPACE-NAME?api-version=2015-11-01-preview"
 
   NEW_CURL_COMMAND=$(sed  "s@OAUTH-TOKEN@${token}@g" <<< $CURL_COMMAND)
@@ -20,11 +18,8 @@ getWorkspaceItemStatus() {
   NEW_CURL_COMMAND=$(sed  "s@RESOURCE-GROUP-NAME@${resource_group}@g" <<< $NEW_CURL_COMMAND)
   NEW_CURL_COMMAND=$(sed  "s@SUBSCRIPTION-ID@${subscription_id}@g" <<< $NEW_CURL_COMMAND)
 
-  echo $NEW_CURL_COMMAND
-
   #Check if we got a 200 back
   result=$(eval curl $NEW_CURL_COMMAND)
-  echo result
   if [[ $result == *"error"* ]]; then
     echo $result
     exit 1
--- utils/getWorkspaceKey.sh
+++ utils/getWorkspaceKey.sh
@@ -11,8 +11,6 @@ getWorkspaceKey() {
   local subscription_id=$4
   local responsevar=$5
 
-  MESSAGE="Creating the Workspace " ; simple_blue_echo
-
   CURL_COMMAND=" -H 'Content-Length: 0' -H 'Host: management.azure.com' -H 'Content-Type: application/json' -H 'Authorization: Bearer OAUTH-TOKEN' -X POST  https://management.azure.com/subscriptions/SUBSCRIPTION-ID/resourcegroups/RESOURCE-GROUP-NAME/providers/Microsoft.OperationalInsights/workspaces/OMS-WORKSPACE-NAME/sharedKeys?api-version=2015-11-01-preview"
 
   NEW_CURL_COMMAND=$(sed  "s@OAUTH-TOKEN@${token}@g" <<< $CURL_COMMAND)
@@ -22,7 +20,6 @@ getWorkspaceKey() {
 
   #Check if we got a 200 back
   result=$(eval curl $NEW_CURL_COMMAND)
-  echo result
   if [[ $result == *"error"* ]]; then
     echo $result
     exit 1
