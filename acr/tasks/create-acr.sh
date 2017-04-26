96823fa6a2d5eeb61c62360df47c77c3c69cd70e
Sani Chabi Yo <chabiyosani@gmail.com>
2017-04-26 18:22:58 -0400

    acr creation task

--- acr/tasks/create-acr.sh
+++ acr/tasks/create-acr.sh
@@ -3,12 +3,12 @@ set -e -x
 
 source azure-oss-demos-ci/utils/pretty-echo.sh
 
-MESSAGE="Getting an access token from AAD" ; simple_blue_echo
-
-az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id"
+az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 
 az acr create -n $registry_name -g $utility_rg -l $location --sku $registry_sku --admin-enabled true
 
-
+az acr credential renew \
+  --name $registry_name \
+  --resource-group $utility_rg
 
 
