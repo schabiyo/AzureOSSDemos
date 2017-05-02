e5b400615cef936b9eb53e6f3cf9b23317eaf53a
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-02 14:19:33 -0400

    task to create acr

--- acr/tasks/create-acr.sh
+++ acr/tasks/create-acr.sh
@@ -5,5 +5,15 @@ source azure-ossdemos-git/utils/pretty-echo.sh
 
 az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 
+#CHeck if the ACR already exist
+
+result=$(eval az acr check-name -n $registry_name)
+
+if [[ $result == *"AlreadyExists"* ]]; then
+  MESSAGE="The registry already exists:" ; simple_blue_echo  
+  exit 0
+fi
+
+
 az acr create -n $registry_name -g $utility_rg -l $location --sku $registry_sku --admin-enabled true
 
