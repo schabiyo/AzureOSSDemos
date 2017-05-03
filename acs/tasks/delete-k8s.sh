b620a7731e0d90cd0d21222be84f0c00ec21a162
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-03 11:17:41 -0400

    task for the web app configuration

--- acs/tasks/delete-k8s.sh
+++ acs/tasks/delete-k8s.sh
@@ -6,6 +6,7 @@ source azure-ossdemos-git/utils/pretty-echo.sh
 az login --service-principal -u $service_principal_id -p $service_principal_secret --tenant $tenant_id
 
 az acs delete -n ossdemok8s$server_prefix -g $acs_rg
+az group delete -n $acs_rg -y
 
 MESSAGE="==>Azure Container Service Successfully deleted" ; simple_green_echo
 
