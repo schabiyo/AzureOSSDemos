c9dfab6cf5dd7c768aaefc9f93f758396e1c131e
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-05 14:39:05 -0400

    updating plan creation

--- infra-provisioning/paas/tasks/create-webplan.sh
+++ infra-provisioning/paas/tasks/create-webplan.sh
@@ -17,7 +17,16 @@ az appservice plan create -g $paas_rg -n webtier-plan --is-linux --number-of-wor
 MESSAGE="==> App Service plan successfully created" ; simple_green_echo
 ## Create the Web app
 az appservice web create -g $paas_rg -p webtier-plan -n $server_prefix-api-nodejs
-MESSAGE="==> Web App template successfully created for API" ; simple_green_echo
+#Map to port 3001
+az appservice web config appsettings update -g $paas_rg -n $server_prefix-api-nodejs --settings PORT=3001
+MESSAGE="==> Web App template successfully created for API-NodeJS" ; simple_green_echo
+
+## Create the Web app
+az appservice web create -g $paas_rg -p webtier-plan -n $server_prefix-web-nodejs
+#Map to port 3000
+az appservice web config appsettings update -g $paas_rg -n $server_prefix-web-nodejs --settings PORT=3000
+MESSAGE="==> Web App template successfully created for WEB-NodeJS" ; simple_green_echo
+
 # Configure the deployment slots for dev staging and production
 az appservice web deployment slot create -n  $server_prefix-api-nodejs -g $paas_rg -s dev
 MESSAGE="==>Deployment slot successfully created for Dev" ; simple_green_echo
@@ -26,5 +35,11 @@ MESSAGE="==>Deployment slot successfully created for Staging" ; simple_green_ech
 az appservice web deployment slot create -n  $server_prefix-api-nodejs -g $paas_rg -s production
 MESSAGE="==>Deployment slot successfully created for Production" ; simple_green_echo
 
+
+# Configure the deployment slots for dev staging and production
+az appservice web deployment slot create -n  $server_prefix-web-nodejs -g $paas_rg -s dev
+MESSAGE="==>Developement Deployment slot successfully created for WEB-NodeJS" ; simple_green_echo
+az appservice web deployment slot create -n  $server_prefix-web-nodejs -g $paas_rg -s staging
+MESSAGE="==>Staging Deployment slot successfully created for WEB-NodeJS" ; simple_green_echo
 
 #TODO Configure a real MySQL DB for the production slot and use MYSQL in APP for the dev slot
