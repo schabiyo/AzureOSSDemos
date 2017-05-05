6972973c05f51e3970b183a89cd06165bd97e408
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-05 00:20:29 -0400

    cleanup

---
+++ devops/deploy-pipeline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PIPELINE_NAME=${1:-ossdemo-devops}
+ALIAS=${2:-syolab}
+CREDENTIALS=${3:-credentials.yml}
+
+echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
