0236cfacfed87eee4195688591f79f6c006a3a80
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-06 00:28:47 -0400

    Removing LB creation

--- infra-provisioning/iaas/tasks/create-networking-resources.sh
+++ infra-provisioning/iaas/tasks/create-networking-resources.sh
@@ -21,19 +21,19 @@ MESSAGE="==>Resource group successfully created"; simple_green_echo
 az network vnet create -g $iaas_rg  -n ossdemo-iaas-vnet --address-prefix 10.0.0.0/16 --subnet-name WebSubnet --subnet-prefix 10.0.0.0/24 -l $location &> /dev/null
 MESSAGE="==>VNET successfully created"; simple_green_echo
 #Create public IP for LB
-az network public-ip create -g $iaas_rg -n lbpip --dns-name $server_prefix"-iaas" --allocation-method Static -l $location &> /dev/null
-MESSAGE="==>Public IP for the Load balancer successfully created"; simple_green_echo
+#az network public-ip create -g $iaas_rg -n lbpip --dns-name $server_prefix"-iaas" --allocation-method Static -l $location &> /dev/null
+#MESSAGE="==>Public IP for the Load balancer successfully created"; simple_green_echo
 #Create a LB
-az network lb create -g $iaas_rg -n IaasLb --vnet-name ossdemo-iaas-vnet  --public-ip-address lbpip &> /dev/null
-MESSAGE="==> Load balancer successfully created"; simple_green_echo
+#az network lb create -g $iaas_rg -n IaasLb --vnet-name ossdemo-iaas-vnet  --public-ip-address lbpip &> /dev/null
+#MESSAGE="==> Load balancer successfully created"; simple_green_echo
 #Create the Availability Set
-az vm availability-set create -n iaaswebas --platform-fault-domain-count 2 --platform-update-domain-count 5 -g $iaas_rg &> /dev/null
-MESSAGE="==>Availability Set successfully created"; simple_green_echo
+#az vm availability-set create -n iaaswebas --platform-fault-domain-count 2 --platform-update-domain-count 5 -g $iaas_rg &> /dev/null
+#MESSAGE="==>Availability Set successfully created"; simple_green_echo
 #Create NAT RUles
-az network lb inbound-nat-rule create -g $iaas_rg --lb-name IaasLb --name ssh1 --protocol tcp --frontend-port 21 --backend-port 22  --frontend-ip-name LoadBalancerFrontEnd &> /dev/null
-az network lb inbound-nat-rule create -g $iaas_rg --lb-name IaasLb --name ssh2 --protocol tcp --frontend-port 23 --backend-port 22  --frontend-ip-name LoadBalancerFrontEnd &> /dev/null
+#az network lb inbound-nat-rule create -g $iaas_rg --lb-name IaasLb --name ssh1 --protocol tcp --frontend-port 21 --backend-port 22  --frontend-ip-name LoadBalancerFrontEnd &> /dev/null
+#az network lb inbound-nat-rule create -g $iaas_rg --lb-name IaasLb --name ssh2 --protocol tcp --frontend-port 23 --backend-port 22  --frontend-ip-name LoadBalancerFrontEnd &> /dev/null
 
-MESSAGE="==>Load Balancer NAT rules successfully created"; simple_green_echo
+#MESSAGE="==>Load Balancer NAT rules successfully created"; simple_green_echo
 # Create a NSG
 az network nsg create -g $iaas_rg -n nsg-iaas-demo -l $location &> /dev/null
 MESSAGE="==>Network Security Group successfully created"; simple_green_echo
@@ -57,19 +57,19 @@ az network nsg rule create -g $iaas_rg --nsg-name nsg-iaas-demo -n http-eshop-de
 
 MESSAGE="==>Network Security rule for port 81  successfully created"; simple_green_echo
 #Create LB Probes
-az network lb probe create -g $iaas_rg --lb-name IaasLb  --name healthprobe --protocol "tcp" --port 80 --interval 15 &> /dev/null
-MESSAGE="==>Load Balancer Probe successfully created"; simple_green_echo
+#az network lb probe create -g $iaas_rg --lb-name IaasLb  --name healthprobe --protocol "tcp" --port 80 --interval 15 &> /dev/null
+#MESSAGE="==>Load Balancer Probe successfully created"; simple_green_echo
 #Create LB Rules
-az network lb rule create -g $iaas_rg --lb-name IaasLb --name lb-web80-rule \
-  --protocol tcp --frontend-port 80 --backend-port 80 \
-  --frontend-ip-name LoadBalancerFrontEnd --backend-pool-name IaasLbbepool &> /dev/null
+#az network lb rule create -g $iaas_rg --lb-name IaasLb --name lb-web80-rule \
+#  --protocol tcp --frontend-port 80 --backend-port 80 \
+#  --frontend-ip-name LoadBalancerFrontEnd --backend-pool-name IaasLbbepool &> /dev/null
 
-MESSAGE="==>Load Balancer Rule for port 80 successfully created"; simple_green_echo
+#MESSAGE="==>Load Balancer Rule for port 80 successfully created"; simple_green_echo
 
-az network lb rule create -g $iaas_rg --lb-name IaasLb --name lb-web81-rule \
-  --protocol tcp --frontend-port 81 --backend-port 81 \
-  --frontend-ip-name LoadBalancerFrontEnd --backend-pool-name IaasLbbepool &> /dev/null
-MESSAGE="==>Load Balancer Rule for port 81 successfully created"; simple_green_echo
+#az network lb rule create -g $iaas_rg --lb-name IaasLb --name lb-web81-rule \
+#  --protocol tcp --frontend-port 81 --backend-port 81 \
+#  --frontend-ip-name LoadBalancerFrontEnd --backend-pool-name IaasLbbepool &> /dev/null
+#MESSAGE="==>Load Balancer Rule for port 81 successfully created"; simple_green_echo
 
 # Init ssh folder and Copy ssh key file 
 #Get the SSH key from the configs adn add it to the ssh folder
