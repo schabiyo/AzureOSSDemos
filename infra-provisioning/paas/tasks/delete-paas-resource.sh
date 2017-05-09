39bb19409d855f87bf3d229f9069c5e8944bb0ac
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-09 10:47:27 -0400

    paas resource deletion

---
+++ infra-provisioning/paas/tasks/delete-paas-resource.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e -x
+
+# Including the utility for echo
+source azure-ossdemos-git/infra-provisioning/utils/pretty-echo.sh
+
+
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id" &>/dev/null
+# 2. switchinh to the default subscription
+
+az account set --subscription "$subscription_id"
+
+#BUILD RESOURCE GROUPS
+az group delete --name $paas_rg  &>/dev/null
+
