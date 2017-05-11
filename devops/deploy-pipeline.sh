0c1d61998c8fcfbe7b03373323106db1ab2b788a
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-10 20:48:16 -0400

    updating infra and devops pipelein

--- devops/deploy-pipeline.sh
+++ devops/deploy-pipeline.sh
@@ -5,3 +5,4 @@ ALIAS=${2:-syolab}
 CREDENTIALS=${3:-credentials.yml}
 
 echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
+fly -t "${ALIAS}" expose-pipeline  -p  "${PIPELINE_NAME}"
