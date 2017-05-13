bf201f1c0123af1f496b59d69a241d3c0e793abd
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-13 03:12:26 +0000

    add code to automatically get the acr pwd

--- devops/deploy-pipeline.sh
+++ devops/deploy-pipeline.sh
@@ -4,5 +4,15 @@ PIPELINE_NAME=${1:-ossdemo-devops}
 ALIAS=${2:-syolab}
 CREDENTIALS=${3:-credentials.yml}
 
+
+#Get the ACR password and update the credentials file
+acr_password=$(az acr credential show -n syoossdemoacr --query passwords[0].value)
+echo $acr_password
+TRIMMED_RESULT="${acr_password%\"}"
+TRIMMED_RESULT="${TRIMMED_RESULT#\"}"
+echo $TRIMMED_RESULT
+
+sed -i -e "s@ACR_ADMIN_PASSWORD:@${TRIMMED_RESULT}@g" credentials.yml
+
 echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
 fly -t "${ALIAS}" expose-pipeline  -p  "${PIPELINE_NAME}"
