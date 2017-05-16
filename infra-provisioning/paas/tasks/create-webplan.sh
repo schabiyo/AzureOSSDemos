36f5c89296f1adefc893d3b227fbd3ef7b57ee2d
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-16 11:48:04 -0400

    remove web plan staging slot

--- infra-provisioning/paas/tasks/create-webplan.sh
+++ infra-provisioning/paas/tasks/create-webplan.sh
@@ -13,7 +13,7 @@ az account set --subscription "$subscription_id"
 #BUILD RESOURCE GROUPS
 az group create --name $paas_rg --location $location &>/dev/null
 
-az appservice plan create -g $paas_rg -n webtier-plan --is-linux --number-of-workers 1 --sku S1 -l westus
+az appservice plan create -g $paas_rg -n webtier-plan --is-linux --number-of-workers 1 --sku S2 -l westus
 MESSAGE="==> App Service plan successfully created" ; simple_green_echo
 ## Create the Web app
 az appservice web create -g $paas_rg -p webtier-plan -n $server_prefix-api-nodejs
@@ -30,8 +30,6 @@ MESSAGE="==> Web App template successfully created for WEB-NodeJS" ; simple_gree
 # Configure the deployment slots for dev staging and production
 az appservice web deployment slot create -n  $server_prefix-api-nodejs -g $paas_rg -s dev
 MESSAGE="==>Deployment slot successfully created for Dev" ; simple_green_echo
-az appservice web deployment slot create -n  $server_prefix-api-nodejs -g $paas_rg -s staging
-MESSAGE="==>Deployment slot successfully created for Staging" ; simple_green_echo
 
 
 # Configure the deployment slots for dev staging and production
