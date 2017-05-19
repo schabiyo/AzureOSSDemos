a27cfc828aa42e90f9f9fd492dfb17b323ff0dd9
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-19 04:43:43 +0000

    add appinsight

---
+++ infra-provisioning/appinsight/tasks/create-appinsight.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x
+
+source azure-ossdemos-git/infra-provisioning/utils/pretty-echo.sh
+
+az login --service-principal -u "$service_principal_id" -p "$service_principal_secret" --tenant "$tenant_id" &> /dev/null
+az account set --subscription "$subscription_id"  &> /dev/null
+
+# Create a resource group.
+az group create --name $utility_rg --location $location &> /dev/null
+
+
+set -x
+
+#Let validate the deployment template first
+
+echo "Validating the template...."
+(
+az group deployment validate \
+    --resource-group $utility_name \
+    --template-file azure-ossdemo-git/infra-provisioning/appinsight/tasks/deploy.json \
+    --parameters "{\"appInsightName\":{\"value\":\"$server_prefix\"}}"
+)
+
+#Start deployment
+
+echo "Starting deployment..."
+(
+	set -x
+	az group deployment create --name web-nodejs  -g $rg_name --template-file web-nodejs/ci/infra/arm/appservice-template.json \
+             --parameters "{\"appInsightName\":{\"value\":\"$webapp_name\"}}" --verbose
+)
+
+if [ $?  == 0 ]; 
+ then
+	echo "Template has been successfully deployed"
+        exit 0
+fi
+
+exit 1
