bc22f94da504fa4981ac2b5810b17be6d086cc3c
Sani Chabi Yo <chabiyosani@gmail.com>
2017-05-19 14:50:14 +0000

    updating configs

--- devops/deploy-pipeline.sh
+++ devops/deploy-pipeline.sh
@@ -5,14 +5,5 @@ ALIAS=${2:-syolab}
 CREDENTIALS=${3:-credentials.yml}
 
 
-#Get the ACR password and update the credentials file
-acr_password=$(az acr credential show -n syoossdemoacr --query passwords[0].value)
-echo $acr_password
-TRIMMED_RESULT="${acr_password%\"}"
-TRIMMED_RESULT="${TRIMMED_RESULT#\"}"
-echo $TRIMMED_RESULT
-
-sed -i -e "s@ACR_ADMIN_PASSWORD:@${TRIMMED_RESULT}@g" credentials.yml
-
 echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
 fly -t "${ALIAS}" expose-pipeline  -p  "${PIPELINE_NAME}"
